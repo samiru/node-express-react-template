f62054cdab8a5d6c084963a26c280bf9fd28e303
Sami Ruokamo <sami.ruokamo@buutti.com>
2023-01-04 14:45:00 +0200

    Revert to commonjs with lowdb

---
+++ backend/src/database/index.ts
@@ -0,0 +1,27 @@
+import { join } from "node:path";
+
+import lowdb from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+
+import { Book } from "../utils/types";
+
+interface Schema {
+  books: Book[];
+}
+
+const connect = () => {
+  const file = join(__dirname, "db.json");
+  const db = lowdb(new FileSync<Schema>(file));
+
+  console.log({ __dirname, file });
+
+  // Read data from JSON file, this will set db.data content
+  db.read();
+
+  // Set some defaults (required if your JSON file is empty)
+  //db.defaults({ books: [] }).write();
+
+  return db;
+};
+
+export default connect;
--- backend/src/services/books.ts
+++ backend/src/services/books.ts
@@ -1,26 +1,25 @@
-import db from "../database/db";
+import connect from "../database";
 import { Book } from "../utils/types";
+
+const db = connect();
 
 export default {
   async getAll() {
-    return db.data?.books || [];
+    return db.get("books").value();
   },
   async get(id: number) {
-    //    return db.get("books").find({ id }).value();
-    return db.data?.books.find((book: Book) => book.id === id);
+    return db.get("books").find({ id }).value();
   },
   async create(book: Book) {
-    //    db.get("books").push(book).write();
-    db.data?.books.push(book);
+    db.get("books").push(book);
     db.write();
     return book;
   },
   async update(id: number, book: Book) {
-    //    db.get("books").find({ id }).assign(book).write();
-    //    db.data?.books.find((book) => book.id === id)?.assign(book);
+    db.get("books").find({ id }).assign(book);
     return book;
   },
   async delete(id: number) {
-    //    db.get("books").remove({ id }).write();
+    db.get("books").remove({ id });
   },
 };
