277e51ee41a463fc11cae81e4cf40a42c35bf0ad
Sami Ruokamo <sami.ruokamo@buutti.com>
2023-01-27 10:35:46 +0200

    Clean up

--- client/src/services/books.ts
+++ client/src/services/books.ts
@@ -4,49 +4,28 @@ import { Book } from "../types";
 const baseURL = process.env.API_BASE_URL || "http://localhost:3001";
 
 const getBooks = async (): Promise<Book[]> => {
-  try {
-    const response = await axios.get(`${baseURL}/api/books`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(`${baseURL}/api/books`);
+  return response.data;
 };
 
 const getBook = async (id: string): Promise<Book> => {
-  try {
-    const response = await axios.get(`${baseURL}/api/book/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(`${baseURL}/api/book/${id}`);
+  return response.data;
 };
 
 const addBook = async (book: Book): Promise<Book> => {
-  try {
-    const response = await axios.post(`${baseURL}/api/books`, book);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.post(`${baseURL}/api/books`, book);
+  return response.data;
 };
 
 const updateBook = async (book: Book): Promise<Book> => {
-  try {
-    const response = await axios.put(`${baseURL}/api/books/${book.id}`, book);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.put(`${baseURL}/api/books/${book.id}`, book);
+  return response.data;
 };
 
 const deleteBook = async (id: string): Promise<Book> => {
-  try {
-    const response = await axios.delete(`${baseURL}/api/books/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  const response = await axios.delete(`${baseURL}/api/books/${id}`);
+  return response.data;
 };
 
 export { getBooks, getBook, addBook, updateBook, deleteBook };
